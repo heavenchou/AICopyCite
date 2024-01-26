48c6f099b59b14a840c25882b6d2118645881750
Heaven Chou <heaven.chou@gmail.com>
2024-01-27 02:48:58 +0800

    Initial Commit

---
+++ AICopyCite/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AICopyCite
+//
+//  Created by Heaven Chou on 2024/1/27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
